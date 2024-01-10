afc8dfd83d86987cff64c6ea8532be1306d0605b
Zain <zain.sohail@gmail.com>
2024-01-10 14:15:46 +0100

    💄 created and styled homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import { defaultConfig } from "next/dist/server/config-shared"
-
+import { defaultConfig } from "next/dist/server/config-shared";
+import styles from "./home.module.css";
+import Image from "next/image";
 const Home = () => {
-  return <div>Homepage </div> 
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn more</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
