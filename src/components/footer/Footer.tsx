b3167fd1b387a844d630e519ac629ae4a0a39d59
Zain <zain.sohail@gmail.com>
2024-01-10 14:16:53 +0100

    created footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,13 @@
-
+import styles from "./footer.module.css";
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.logo}>zainDev</div>
+      <div className={styles.text}>
+        zain creative thoughts agency © All rights reserved
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
