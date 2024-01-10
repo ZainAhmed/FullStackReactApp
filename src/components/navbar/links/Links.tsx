cbbd6fc3f486f1a1e0d7637217c8de66772d199d
Zain <zain.sohail@gmail.com>
2024-01-10 12:53:07 +0100

    created menu navbar

--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,40 +1,56 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 function Links() {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
-
+  const [isOpen, setOpen] = useState(false);
   const isAdmin = true;
   const isLoggedin = true;
   return (
-    <div className={styles.links}>
-      {links.map((link, index) => (
-        <NavLink item={link} key={index} />
-      ))}
-      {isLoggedin ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link, index) => (
+          <NavLink item={link} key={index} />
+        ))}
+        {isLoggedin ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {isOpen && (
+        <div className={styles.mobileLinks}>
+          {links.map((link, index) => (
+            <NavLink item={link} key={index} />
+          ))}
+        </div>
       )}
     </div>
   );
