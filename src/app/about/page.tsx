88325e0635261c0387b959b77b7e2718584e9805
Zain <zain.sohail@gmail.com>
2024-01-10 17:54:18 +0100

    created about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,39 @@
-
-
+import Image from "next/image";
+import styles from "./about.module.css";
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger,bolder,braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger,bolder,braver and better. We
+          believe in good ideas flexibility and precision. We are the world's
+          best consulting & finance solution provider. We provide a wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>24 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/about.png" alt="" fill className={styles.img} />
+      </div>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
